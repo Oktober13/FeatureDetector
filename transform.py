fe60cbbb526bc3eba505bd3b3eef470c787d61f8
lzuehsow <lydiazuehsow@gmail.com>
2018-12-31 19:59:14 -0500

    Fixes skew

--- transform.py
+++ transform.py
@@ -2,7 +2,7 @@
 
 """ 
 Original code for rotation belongs to Alex & Gareth Rees, see https://codereview.stackexchange.com/questions/41688/rotating-greyscale-images.
-Code for scaling and skew written by L. Zuehsow (Oktober13)
+Code for scaling and shear written by L. Zuehsow (Oktober13)
 The transform matrices act on individual cells from the original source image, which are treated as "points."
 It's important to only act on cells in the source image and determine where they should be placed in the destination image. 
 The reverse operation is prone to indexing issues when the destination image is a different size from the original.
@@ -75,13 +75,14 @@ class Transform(object):
 	@staticmethod
 	def scaleCoords(coord_list, scale):
 		""" Scale the coordinates of the four corners of the image. """
+		scaleX, scaleY = scale[0][0], scale[1][1]
 		x, y = coord_list[:][0], coord_list[:][1] # Vector of all x and all y coordinates for corners.
-		sx, sy = np.dot(x,1.0/scale), np.dot(y, 1.0/scale)
+		sx, sy = np.dot(x,1.0/scaleX), np.dot(y, 1.0/scaleY)
 		return sx, sy
 
 	@staticmethod
 	def scale(src, scale, fill=255):
-		""" Scale an image up or down in size. """
+		""" Scale an image horizontally or vertically. """
 
 		height, width, channels = src.shape
 
@@ -89,8 +90,8 @@ class Transform(object):
 		corners = np.transpose([[0,0],[width,0],[width,height],[0,height]])
 
 		# Determine dimensions of destination image by finding extremes.
-		newWidth = int(scale * int(np.ceil(corners[0].max() - corners[0].min())))
-		newHeight = int(scale * int(np.ceil(corners[1].max() - corners[1].min())))
+		newWidth = int(scale[0][0] * (int(np.ceil(corners[0].max() - corners[0].min()))))
+		newHeight = int(scale[1][1] * (int(np.ceil(corners[1].max() - corners[1].min()))))
 
 		# Iterate over image to form grid of coordinates of pixels in destination image.
 		dx, dy = np.meshgrid(np.arange(newWidth), np.arange(newHeight))
@@ -114,17 +115,22 @@ class Transform(object):
 		dest[dy[~mask], dx[~mask]] = fill
 		return dest
 
+
 	@staticmethod
-	def skewCoords(coord_list, scale):
-		""" Scale the coordinates of the four corners of the image. """
-		scaleX, scaleY = skew[0][0], skew[1][1]
+	def shearCoords(coord_list, scale, topRight = [[0][0]]):
+		""" Shear the coordinates of the four corners of the image. """
+		shearX, shearY = scale[0][1], scale[1][0]
 		x, y = coord_list[:][0], coord_list[:][1] # Vector of all x and all y coordinates for corners.
-		sx, sy = np.dot(x,1.0/scaleX), np.dot(y, 1.0/scaleY)
+		sx, sy = x + y * shearX, y + x * shearY
+		if shearX > 0:
+			sx = np.add(sx,-topRight[0])
+		if shearY > 0:
+			sy = np.add(sy,-topRight[1])
 		return sx, sy
 
 	@staticmethod
-	def skew(src, skew, fill=255):
-		""" Skew an image horizontally or vertically. """
+	def shear(src, scale, fill=255):
+		""" Scale an image horizontally or vertically. """
 
 		height, width, channels = src.shape
 
@@ -132,14 +138,23 @@ class Transform(object):
 		corners = np.transpose([[0,0],[width,0],[width,height],[0,height]])
 
 		# Determine dimensions of destination image by finding extremes.
-		newWidth = int(skew[0][0] * int(np.ceil(corners[0].max() - corners[0].min())))
-		newHeight = int(skew[1][1] * int(np.ceil(corners[1].max() - corners[1].min())))
+		if shear[0][1] != 0:
+			print shear[1][0]
+			newWidth = int(np.ceil(corners[0].max() - corners[0].min())) + int(shear[0][1] * (int(np.ceil(corners[1].max() - corners[1].min()))))
+		else:
+			newWidth = int(np.ceil(corners[0].max() - corners[0].min()))
+		if shear[1][0] != 0:
+			newHeight = int(np.ceil(corners[1].max() - corners[1].min())) + int(shear[1][0] * (int(np.ceil(corners[0].max() - corners[0].min()))))
+		else:
+			newHeight = int(np.ceil(corners[1].max() - corners[1].min()))
+
+		print newWidth, newHeight, shear[0][1], shear[1][0]
 
 		# Iterate over image to form grid of coordinates of pixels in destination image.
 		dx, dy = np.meshgrid(np.arange(newWidth), np.arange(newHeight))
 		pixels = [dx + corners[0].min(), dy + corners[1].min()] # Moves into frame
 
-		sx, sy = t.skewCoords(pixels, scale)
+		sx, sy = t.shearCoords(pixels, shear, [newWidth-width,newHeight-height])
 
 		# Select nearest neighbour pixel. Index must be integer.
 		sx, sy = sx.round().astype(int), sy.round().astype(int)
@@ -158,20 +173,20 @@ class Transform(object):
 		return dest
 
 	@staticmethod
-	def transform(src, angle = 0, scale = 0, skew = None):
+	def transform(src, angle = 0, scale = None, shear = None):
 		""" Wrapper function, performs requested transforms. """
 		dst = src
 
-		if scale is not 1:
-			if scale > 0:
-				dst = t.scale(src, scale)
+		if scale is not None or np.identity(2):
+			if scale[0][0] > 0 and scale[1][1] > 0:
+				dst = t.scale(dst, scale)
 			else:
 				print "INVALID INPUT: Negative or zero scale is not allowed."
-		if skew is not None or np.identity(2):
-			if skew[0][0] > 0 and skew[1][1] > 0:
-				dst = t.skew(dst,skew)
+		if shear is not None or np.identity(2):
+			if shear[0][0] > 0 and shear[1][1] > 0:
+				dst = t.shear(dst,shear)
 			else:
-				print "INVALID INPUT: Negative or zero skews are not allowed."
+				print "INVALID INPUT: Negative or zero shear is not allowed."
 		if angle is not 0:
 			origin = (int(dst.shape[1] / 2), int(dst.shape[0] / 2)) # y, x
 			dst = t.rotate_image(dst, angle * np.pi / 180, origin)
@@ -186,13 +201,13 @@ class Transform(object):
 
 if __name__ == "__main__":
 	angle = 45
-	scale = 1.5
-	skew = np.array([[2,0],[0,1]])
+	scale = np.array([[2,0],[0,1]])
+	shear = np.array([[1,0.5],[0,1]])
 
 	t = Transform()
 
 	src = cv2.imread("frisk.jpg")
 
-	fin = t.transform(src, angle, scale, skew)
+	fin = t.transform(src, angle, scale, shear)
 	# dst = t.rotate_image(src, angle * np.pi / 180, (100, 100))
 	t.display(src, fin)
